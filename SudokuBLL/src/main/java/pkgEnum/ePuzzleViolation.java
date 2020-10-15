d6683d6c10fe6536aa2b9513c2a3bb5c9a17b3ed
lorrhahitchner <lorrhahitchner@10.0.0.105>
2020-10-15 16:09:13 -0400

    ePuzzleViolation method

--- SudokuBLL/src/main/java/pkgEnum/ePuzzleViolation.java
+++ SudokuBLL/src/main/java/pkgEnum/ePuzzleViolation.java
@@ -3,4 +3,14 @@ package pkgEnum;
 public enum ePuzzleViolation {
 
 	DupRow, DupCol, DupRegion, InvalidValue, ContainsZero, MissingZero;
+	
+	/**
+	 * Returns the enum constant of this type with the specified name. The string must match exactly an identifier used to declare an enum constant in this type. 
+	 * (Extraneous whitespace characters are not permitted.)
+	 * @param name
+	 * @return
+	 */
+	public static ePuzzleViolation valueOf​(java.lang.String name) {
+		return null;
+	}
 }
